a385e82e51c9c3bf478bf98e10e0fe3bdd5bba8f
trueshura <trueshura@mail.ru>
2024-03-18 14:12:34 +0700

    Fix RPC URL handling

--- misc/rpc-client.js
+++ misc/rpc-client.js
@@ -1,47 +1,56 @@
 const buffer = require('buffer/').Buffer;
 const axios = require('axios').default;
 const {assert} = require('./misc');
+
+const cUrl = URL ? URL : require('url').URL;
+
 module.exports = class Client {
-    constructor({url, port, auth}) {
-        assert(this._url = url, 'Specify url');
-        this._port = port;
+  constructor({url, port, auth}) {
+    assert(url, 'Specify url');
 
-        // TODO add port support
+    const bSsl = port === 443;
+    const strProtoPrefix = bSsl ? 'https' : 'http';
+    const strTempUrl = url.startsWith('http') ? url : `${strProtoPrefix}://${url}`;
+    const myUrl = new cUrl(strTempUrl);
+    myUrl.port = port;
+    this._url = myUrl.toString();
 
-        const authMixin = auth ? {
-            Authorization: `Basic ${buffer.from(auth).toString('base64')}`
-        } : {};
+    const authMixin = auth ? {
+      Authorization: `Basic ${buffer.from(auth).toString('base64')}`
+    } : {};
 
-        this._baseCfg = {
-            method: 'post',
-            url: this._url,
-            timeout: 20000,
-            headers: {
-                'Content-Type': 'application/json',
-                ...authMixin
-            }
-        };
-    }
+    this._baseCfg = {
+      method: 'post',
+      url: this._url,
+      timeout: 20000,
+      headers: {
+        'Content-Type': 'application/json',
+        ...authMixin
+      }
+    };
 
-    set client(val) {
+    this._client = axios.create(this._baseCfg);
+  }
 
-    }
+  set client(client) {
+    this._client = client;
+  }
 
-    async request(strMethod, objParams) {
-        const resp = await axios.post(
-            this._url,
-            this._createData(strMethod, objParams),
-            this._baseCfg
-        );
-        return resp.data;
-    }
+  async request(strMethod, objParams) {
+    const resp = await this._client.post(
+      this._url,
+      this._createData(strMethod, objParams),
+      this._baseCfg
+    );
+    return resp.data;
+  }
 
-    _createData(strMethod, objParams = {}, seqId = 0) {
-        return {
-            "jsonrpc": "2.0",
-            "method": strMethod,
-            "params": objParams,
-            "id": seqId
-        };
-    }
+  _createData(strMethod, objParams = {}, seqId = 0) {
+    return {
+      "jsonrpc": "2.0",
+      "method": strMethod,
+      "params": objParams,
+      "id": seqId
+    };
+  }
 };
--- tests/rpc.spec.js
+++ tests/rpc.spec.js
@@ -3,42 +3,69 @@
 const {describe, it} = require('mocha');
 const {assert} = require('chai');
 const RPC = require('../misc/rpc-client');
+const MockAdapter = require('axios-mock-adapter');
 
 const sleep = (delay) => {
-    return new Promise(resolve => {
-        setTimeout(() => resolve(), delay);
-    });
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), delay);
+  });
 };
 
 describe('RPC (via axios)', () => {
-    before(async function() {
-        this.timeout(15000);
+  before(async function() {
+    this.timeout(15000);
+  });
+
+  after(async function() {
+    this.timeout(15000);
+  });
+
+  it('should create instance', async () => {
+    const rpc = new RPC({url: 'https://ya.ru/', auth: 'login:pass'});
+  });
+
+  it('should create https instance', async () => {
+    const rpc = new RPC({url: 'localhost', port: 443});
+
+    assert.equal(rpc._url, 'https://localhost/');
+  });
+
+  it('should create localhost instance', async () => {
+    const rpc = new RPC({url: 'localhost', port: 18222});
+
+    assert.equal(rpc._url, 'http://localhost:18222/');
+  });
+
+  it('should inject port into URL', async () => {
+    const port = 18222;
+    const rpc = new RPC({url: 'https://ya.ru/test', port, auth: 'login:pass'});
+
+    assert.equal(rpc._url, `https://ya.ru:${port}/test`);
+  });
+
+  it('should send request', async () => {
+    const method = 'getBlock';
+    const objRequest = {
+      "strBlockHash": "cfe8271e30208557c3abd30919c858b2853ced063920ade5c67bd7ad9e369383"
+    };
+    const port = 18222;
+    const rpc = new RPC({url: 'https://rpc-dv-1.ubikiri.com/', port, auth: 'cilTest:d49c1d2735536baa4de1cc6'});
+    const mock = new MockAdapter(rpc._client);
+    const objFakeResp = {id: 0, data: 'fake'};
+//    mock.onPost().reply(200, objFakeResp);
+    mock.onPost().reply(async (config) => {
+      const data = JSON.parse(config.data);
+      if (
+        config.url === `https://rpc-dv-1.ubikiri.com:${port}/` &&
+        data.method === method &&
+        JSON.stringify(data.params) === JSON.stringify(objRequest)) {
+        return [200, objFakeResp];
+      }
+      return [500];
     });
 
-    after(async function() {
-        this.timeout(15000);
-    });
+    const resp = await rpc.request(method, objRequest);
 
-    it('should create instance', async () => {
-        const rpc = new RPC({url: 'https://ya.ru/', auth: 'login:pass'});
-    });
-
-    it.skip('should send request', async () => {
-        const rpc = new RPC({url: 'https://rpc-dv-1.ubikiri.com/', auth: 'cilTest:d49c1d2735536baa4de1cc6'});
-
-        const result = await rpc.request('getBlock', {
-            "strBlockHash": "cfe8271e30208557c3abd30919c858b2853ced063920ade5c67bd7ad9e369383"
-        });
-
-        console.dir(result, {colors: true, depth: null});
-    });
-
-    it.skip('should send request 2', async () => {
-        const rpc = new RPC({url: 'https://rpc-dv-1.ubikiri.com/', auth: 'cilTest:d49c1d2735536baa4de1cc6'});
-
-        const result = await rpc.request('getTips', {});
-
-        console.dir(result, {colors: true, depth: null});
-    });
-
+    assert.deepEqual(resp, objFakeResp);
+  });
 });
