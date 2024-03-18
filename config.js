df8f04669f2ca80ab3338855dd7bad6786bf1fd4
trueshura <trueshura@mail.ru>
2024-03-18 14:13:02 +0700

    Fix config names

--- config.js
+++ config.js
@@ -1,34 +1,31 @@
 let apiUrl;
 let port;
-let conciliumTwoAddr;
-let conciliumTwoRpcUser;
-let conciliumTwoRpcPass;
+let rpcAddr;
+let rpcUser;
+let rpcPass;
 
 if (process.env.NODE_ENV === 'Devel') {
   apiUrl = 'https://test-explorer.ubikiri.com/api/';
   port = 18222;
-  conciliumTwoAddr = 'rpc-dv-1.ubikiri.com';
-  conciliumTwoRpcUser = 'cilTest';
-  conciliumTwoRpcPass = 'd49c1d2735536baa4de1cc6';
+  rpcAddr = 'rpc-dv-1.ubikiri.com';
+  rpcUser = 'cilTest';
+  rpcPass = 'd49c1d2735536baa4de1cc6';
 } else {
   apiUrl = 'https://explorer.ubikiri.com/api/';
   port = 443;
-  conciliumTwoAddr = 'rpc-pr-1.ubikiri.com';
-  conciliumTwoRpcUser = 'ubikiri';
-  conciliumTwoRpcPass = '622ca88c4e2ea80217';
+  rpcAddr = 'rpc-pr-1.ubikiri.com';
+  rpcUser = 'ubikiri';
+  rpcPass = '622ca88c4e2ea80217';
 }
 
 module.exports = {
-  RPC_ADDRESS: conciliumTwoAddr,
+  RPC_ADDRESS: rpcAddr,
   RPC_PORT: port,
-  RPC_USER: conciliumTwoRpcUser,
-  PRC_PASS: conciliumTwoRpcPass,
+  RPC_USER: rpcUser,
+  PRC_PASS: rpcPass,
 
   // на сколько частей побить сумму (для того, чтобы не ждать стабильности блоков)
   DEFAULT_NUM_OUTPUTS: 1,
   FEE_PER_INPUT_OUTPUT: parseInt(4000 / 1024 * 112),
   API_URL: apiUrl,
-  conciliumTwoAddr,
-  conciliumTwoRpcUser,
-  conciliumTwoRpcPass
 };
